15b7ba4109e425847d463f3017dd4b86c9d3ec65
Vokan Kocak <vokankocak@gmail.com>
2020-07-30 20:50:25 +0200

    Composant 1

--- src/app/mock/collegues.mock.ts
+++ src/app/mock/collegues.mock.ts
@@ -1,4 +1,4 @@
 import { Collegue } from '../models/Collegue';
 
-export const collegueMock: Collegue = new Collegue ('NEO404', 'Loinvoyant', 'Fitz', 'Loinvoyant.Fite@gmail.com', '1450-11-11', '/.asserts/Fitz.JPG');
+export const collegueMock: Collegue = new Collegue ('NEO404', 'Loinvoyant', 'Fitz', 'Loinvoyant.Fite@gmail.com', '1450-11-11', '/.asserts/Fitz.jpg');
 
