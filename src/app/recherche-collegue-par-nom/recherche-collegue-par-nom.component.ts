3091aade76a33440a26c9f18dd549b6437e28990
Vokan Kocak <vokankocak@gmail.com>
2020-08-04 23:15:32 +0200

    #3

--- src/app/recherche-collegue-par-nom/recherche-collegue-par-nom.component.ts
+++ src/app/recherche-collegue-par-nom/recherche-collegue-par-nom.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RechercheCollegueParNomComponent implements OnInit {
 
   rechercher = false;
-
+  matriculesMock;
   constructor() { }
 
   ngOnInit(): void {
